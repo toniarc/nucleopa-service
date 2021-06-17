e1e1048c356d4757d582695b73e933356f80aa47
Antonio Junior <antonio.junior@prodepa.pa.gov.br>
2021-06-16 22:43:03 -0300

    Correção consulta pessoa fisica

--- nucleopa-jpa-adapter/src/main/java/br/gov/pa/prodepa/nucleopa/jpa/repository/PessoaFisicaJpaRepository.java
+++ nucleopa-jpa-adapter/src/main/java/br/gov/pa/prodepa/nucleopa/jpa/repository/PessoaFisicaJpaRepository.java
@@ -17,7 +17,7 @@ import br.gov.pa.prodepa.nucleopa.jpa.entity.PessoaFisicaEntity;
 public interface PessoaFisicaJpaRepository extends JpaRepository<PessoaFisicaEntity, Long>{
 
 	@Query(" select p.id as id , p.nome as nome, pf.cpf as cpf "
-			+ " from PessoaFisica pf "
+			+ " from PessoaFisicaEntity pf "
 			+ " join pf.pessoa p "
 			+ " where p.id in(:ids) "
 			+ " order by p.id ")
